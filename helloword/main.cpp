8fdc1b0f6fae05f82692c4ca3e294e7b17352ddf
cyniu88 <cyniu88>
2015-09-25 18:16:12 +0200

    25wrzesnia

---
+++ helloword/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int h;
+    char gg = 'H';
+    cout << "Hello World! " <<  gg << endl;
+    std::cin >> h;
+    return 0;
+}
+
--- media_cyniu/main.cpp
+++ media_cyniu/main.cpp
@@ -1,20 +1,20 @@
-    #define BOARD RASPBERRY_PI
+#define BOARD RASPBERRY_PI
 
-    #include "gnublin.h"
+#include "gnublin.h"
 
-     
-    int main()
-    {
-       gnublin_gpio gpio;
-     
-       gpio.pinMode(3,OUTPUT);
-     
-       while(1){
-         gpio.digitalWrite(3,HIGH);
-         sleep(2);
-         gpio.digitalWrite(3,LOW);
-         sleep(2);
-       }
+
+int main()
+{
+    gnublin_gpio gpio;
+
+    gpio.pinMode(3,OUTPUT);
+
+    while(1){
+        gpio.digitalWrite(3,HIGH);
+        sleep(2);
+        gpio.digitalWrite(3,LOW);
+        sleep(2);
     }
+}
 
 
