34f1a8a609cfdd74dae9e66b1ec572bc7a2a10d3
cyniu <cyniu88@wp.pl>
2017-09-05 15:28:02 +0200

    czyszczenie eventow

--- iDom_server_OOP/src/command/commandClass/command_event.cpp
+++ iDom_server_OOP/src/command/commandClass/command_event.cpp
@@ -13,12 +13,17 @@ std::string command_event::execute(std::vector<std::string> &v, thread_data *my_
     if (v.size() == 2){
             return my_data->myEventHandler.run(v[1])->getEvent();
     }
+    if (v.size() == 3 && v[2]=="clear"){
+            my_data->myEventHandler.run(v[1])->clearEvent();
+            return  "event " +v[1]+ " has been cleared!";
+    }
     return my_data->myEventHandler.help();
 }
 
 std::string command_event::help()
 {
     std::string help = "event- show actual  event (all)\n";
-    help.append("event <name> - show one event");
+    help.append("event <name> - show one event\n");
+    help.append("event <name> clear - clear event <name>");
     return help.append("\n");
 }
