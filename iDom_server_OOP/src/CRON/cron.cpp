dafe9f578fa3023eb87b5e60215da5aa966d2f01
cyniu <cyniu88@wp.pl>
2017-11-03 09:12:07 +0100

    zmina dzilania cron'u na bardziej efektywny

--- iDom_server_OOP/src/CRON/cron.cpp
+++ iDom_server_OOP/src/CRON/cron.cpp
@@ -1,5 +1,5 @@
 #include "cron.hpp"
-// api key  47XSQ0J9CPJ4BO2O
+
 CRON::CRON(thread_data *my_data)
 {
     this->my_data = my_data;
@@ -11,32 +11,61 @@ void CRON::run()
 {
     time_t act_time;
     struct tm * act_date;
-    int min =0 ;
-    char buffer[5];
+    int min = 0 ;
 
     while (useful_F::go_while) {
         sleep(10);
         time(&act_time);
         act_date = localtime(&act_time);
 
-        if (act_date->tm_min % 15 == 0 )
-        {
-            if (check_temperature==TRUE)
-            {
-                my_data->main_iDomTools->send_temperature_thingSpeak();
-            }
-            check_temperature=FALSE;
-        }
-        else
-        {
-            check_temperature=TRUE;
-        }
-
         if (min != act_date->tm_min  )
         {
-            strftime(buffer,5,"%H%M",act_date);
-            useful_F::send_to_arduino_clock(my_data,buffer);
+            runEveryOne1min(act_date);
+            if (act_date->tm_min % 5 == 0 )
+            {
+                runEveryOne5min();
+            }
+            if (act_date->tm_min % 15 == 0 )
+            {
+                runEveryOne15min();
+            }
+            if (act_date->tm_min % 30 == 0 )
+            {
+                runEveryOne30min();
+            }
+            if (act_date->tm_min  == 0 )
+            {
+                runEveryOne1h();
+            }
             min = act_date->tm_min;
         }
     }
+}
+
+void CRON::runEveryOne1min(struct tm *act_date)
+{
+    char buffer[5];
+    strftime(buffer,5,"%H%M",act_date);
+    useful_F::send_to_arduino_clock(my_data,buffer);
+}
+
+void CRON::runEveryOne5min()
+{
+   // printf("co 5 minut! \n");
+}
+
+void CRON::runEveryOne15min()
+{
+  //  printf("co 15 minut! \n");
+    my_data->main_iDomTools->send_temperature_thingSpeak();
+}
+
+void CRON::runEveryOne30min()
+{
+   // printf("co 30 minut! \n");
+}
+
+void CRON::runEveryOne1h()
+{
+  //  printf("co godzine! \n");
 }
--- iDom_server_OOP/src/CRON/cron.hpp
+++ iDom_server_OOP/src/CRON/cron.hpp
@@ -2,7 +2,6 @@
 #define CRON_HPP
 #include <iostream>
 #include <time.h>
-#include <curl/curl.h>
 #include <string>
 #include"../functions/functions.h"
 
@@ -15,6 +14,11 @@ private:
     bool check_temperature;
     thread_data * my_data;
     void send_time();
+    void runEveryOne1min(struct tm *act_date);
+    void runEveryOne5min();
+    void runEveryOne15min();
+    void runEveryOne30min();
+    void runEveryOne1h();
 };
 
 #endif // CRON_HPP
