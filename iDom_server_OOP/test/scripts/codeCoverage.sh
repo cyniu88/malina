1068084f3918556483e91be3b9d4d2d0c5106034
cyniu88-debian <cyniu88@debian.pl>
2018-09-24 23:01:37 +0200

    nowa wersja libs

--- iDom_server_OOP/test/scripts/codeCoverage.sh
+++ iDom_server_OOP/test/scripts/codeCoverage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-../build-iDom_TESTs-CMAKE-clang-Debug/iDom_TESTs-CMAKE
+../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE
 echo "generuje raport"
-llvm-profdata-3.9  merge -o ../build-iDom_TESTs-CMAKE-clang-Debug/test.profdata ../build-iDom_TESTs-CMAKE-clang-Debug/default.profraw
+llvm-profdata-3.9  merge -o ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/default.profraw
 echo "tworze html"
-llvm-cov-3.9  report ../build-iDom_TESTs-CMAKE-clang-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-clang-Debug/test.profdata -use-color > /tmp/coverage.html
-llvm-cov-3.9 show ../build-iDom_TESTs-CMAKE-clang-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-clang-Debug/test.profdata  -filename-equivalence -use-color --format html >> /tmp/coverage.html
+llvm-cov-3.9  report ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata -use-color > /tmp/coverage.html
+llvm-cov-3.9 show ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata  -filename-equivalence -use-color --format html >> /tmp/coverage.html
 echo "wysylam html na server"
 scp -P 11 /tmp/coverage.html  root@192.168.1.1:/www_php/coverage.html
 echo "koniec"
