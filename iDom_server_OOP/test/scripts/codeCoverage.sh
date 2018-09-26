b08dbbd94bce10b5869385f2f84b3b22e3b3dcb4
cyniu88-debian <cyniu88@debian.pl>
2018-09-26 23:11:01 +0200

    poprawka codeCov skryptu

--- iDom_server_OOP/test/scripts/codeCoverage.sh
+++ iDom_server_OOP/test/scripts/codeCoverage.sh
@@ -2,8 +2,10 @@
 ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE
 echo "generuje raport"
 llvm-profdata-3.9  merge -o ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/default.profraw
-echo "tworzellvm-cov-3.9  report ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata -use-color > /tmp/coverage.html
+echo "tworze html"
+
 llvm-cov-3.9 show ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata  -filename-equivalence -use-color --format html > /tmp/coverage.html
 echo "wysylam html na server"
-scp -P 11 /tmp/coverage.html  root@cyniu88.no-ip.pl:/www_php/coverage.html
+scp -P 11 /tmp/coverage.html  root@192.168.1.1:/www_php/coverage.html
 echo "koniec"
+llvm-cov-3.9  report ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata -use-color 
