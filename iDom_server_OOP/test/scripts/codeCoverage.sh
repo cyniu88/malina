3f13edf596b1990aeec9afe2820c0b181ab13b64
cyniu88-debian <cyniu88@debian.pl>
2018-09-28 22:43:49 +0200

    dodano raport pokrycia kodu

--- iDom_server_OOP/test/scripts/codeCoverage.sh
+++ iDom_server_OOP/test/scripts/codeCoverage.sh
@@ -9,3 +9,6 @@ echo "wysylam html na server"
 scp -P 11 /tmp/coverage.html  root@192.168.1.1:/www_php/coverage.html
 echo "koniec"
 llvm-cov-3.9  report ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata -use-color 
+
+llvm-cov-3.9 show ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata  -filename-equivalence -use-color --format text > coverage.txt
+llvm-cov-3.9  report ../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Desktop_Qt_5_11_2_GCC_64bit-Debug/test.profdata -use-color  > report.txt
