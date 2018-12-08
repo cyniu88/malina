ebfdfa8b8bea3be19aab11734f7541d70b694f99
cyniu88 <cyniu88@wp.pl>
2018-12-08 21:42:40 +0100

    poprawka code coverage

--- iDom_server_OOP/test/scripts/codeCoverage.sh
+++ iDom_server_OOP/test/scripts/codeCoverage.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/iDom_TESTs-CMAKE
+../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/iDom_TESTs-CMAKE
 echo "generuje raport"
-llvm-profdata-3.9  merge -o ../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/test.profdata ../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/default.profraw
+llvm-profdata-3.9  merge -o ../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/test.profdata ../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/default.profraw
 echo "tworze html"
 
-llvm-cov-3.9 show ../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/test.profdata  -filename-equivalence -use-color --format html > /tmp/coverage.html
+llvm-cov-3.9 show ../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/test.profdata  -filename-equivalence -use-color --format html > /tmp/coverage.html
+
 echo "wysylam html na server"
 scp -P 11 /tmp/coverage.html  root@cyniu88.no-ip.pl:/www_php/coverage.html
 echo "koniec"
-llvm-cov-3.9  report ../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/test.profdata -use-color | sed -e 's/\/home\/cyniu\/GIT\/malina\/iDom_server_OOP\///g' 
+llvm-cov-3.9  report ../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/test.profdata -use-color | sed -e 's/\/home\/cyniu\/GIT\/malina\/iDom_server_OOP\///g' 
 
-llvm-cov-3.9 show ../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/test.profdata  -filename-equivalence  --format text > coverage.txt
-llvm-cov-3.9 report ../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Zaimportowany_zestaw_narz_dzi_tymczasowy-Debug/test.profdata  > report.txt
+llvm-cov-3.9 show ../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/test.profdata  -filename-equivalence  --format text > coverage.txt
+llvm-cov-3.9 report ../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/iDom_TESTs-CMAKE -instr-profile=../build-iDom_TESTs-CMAKE-Imported_Kit-debuge/test.profdata  > report.txt
 sed -i 's/\/home\/cyniu\/GIT\/malina\/iDom_server_OOP\///g' report.txt
 cp /tmp/coverage.html  ./coverage.html
