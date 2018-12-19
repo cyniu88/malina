9968926dc15e4ab667b3f6968b745f7ce9b84033
Krzysztof Druciak <krzysztof.druciak@ericsson.com>
2018-12-19 14:24:04 +0100

    14 w idom

--- koko/main.cpp
+++ koko/main.cpp
@@ -6,5 +6,5 @@ int main()
 {
     std::time_t t = std::time(nullptr);
     std::tm tm = *std::localtime(&t);
-    std::cout << "ja_JP: " << std::put_time(&tm, "%c %Z") << '\n';
+    std::cout << "chyba dziala ja_JP: " << std::put_time(&tm, "%c %Z") << '\n';
 }
