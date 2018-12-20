1cd7284757e36b2cd00d8ddb81e5fbad06f49a63
Krzysztof Druciak <krzysztof.druciak@ericsson.com>
2018-12-20 11:39:28 +0100

    logi

--- iDom_server_OOP/src/c_irda_logic/test/c_irda_logic_BT.cpp
+++ iDom_server_OOP/src/c_irda_logic/test/c_irda_logic_BT.cpp
@@ -114,16 +114,19 @@ TEST_F(c_irda_logic_fixture, irdaMPD)
     test_irda->_add(PILOT_KEY::KEY_2); //default
     EXPECT_EQ(test_Q._size(), 0);
 }
-/*
+
 TEST_F(c_irda_logic_fixture, sleeper_Logic_EXIT)
-{
+{ puts("cyniu 1");
     test_my_data.sleeper = 0;
     EXPECT_EQ(test_my_data.sleeper, 0);
     test_irda->_add(PILOT_KEY::KEY_MENU);
+    puts("cyniu 2");
     do {
         test_irda->_add(PILOT_KEY::KEY_VOLUMEUP);
+        puts("cyniu 2.5");
     } while(TEST_DATA::LCD_print != "5.SLEEPer");
     do {
+        puts("cyniu 2,75");
         test_irda->_add(PILOT_KEY::KEY_VOLUMEDOWN);
     } while(TEST_DATA::LCD_print != "5.SLEEPer");
     test_irda->_add(PILOT_KEY::KEY_OK);
@@ -134,7 +137,7 @@ TEST_F(c_irda_logic_fixture, sleeper_Logic_EXIT)
     test_irda->_add(PILOT_KEY::KEY_EXIT);
     EXPECT_EQ(test_my_data.sleeper, 0);
 }
-
+/*
 TEST_F(c_irda_logic_fixture, sleeper_Logic_OK)
 {
     std::cout << test_my_data.main_REC->listAllName() << std::endl;
