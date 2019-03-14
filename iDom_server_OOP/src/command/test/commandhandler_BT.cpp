b9dce37e0fe2b53946ac11b6fe6fa50219c320ab
Krzysztof Druciak <krzysztof.druciak@ericsson.com>
2019-03-14 09:01:05 +0100

    pokrycie kodu

--- iDom_server_OOP/src/command/test/commandhandler_BT.cpp
+++ iDom_server_OOP/src/command/test/commandhandler_BT.cpp
@@ -1,4 +1,5 @@
 #include "../commandhandlerrs232.h"
+#include "../commandhandlermqtt.h"
 #include "../../iDomTools/test/iDomTools_fixture.h"
 
 class command_handler_rs232_fixture : public iDomTOOLS_ClassTest
@@ -29,4 +30,38 @@ TEST_F(command_handler_rs232_fixture, main)
     test_v.push_back("ok");
     auto ret = test_chRS232->run(test_v, &test_my_data);
     EXPECT_STREQ(ret.c_str(), "\nEND\n");
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+class command_handler_mqtt_fixture : public iDomTOOLS_ClassTest
+{
+public:
+    command_handler_mqtt_fixture()
+    {
+
+    }
+
+protected:
+    std::unique_ptr<CommandHandlerMQTT> test_chMQTT;
+    std::vector<std::string> test_v;
+    void SetUp() final
+    {
+        iDomTOOLS_ClassTest::SetUp();
+        test_chMQTT = std::make_unique<CommandHandlerMQTT>();
+    }
+
+    void TearDown() final
+    {
+        iDomTOOLS_ClassTest::TearDown();
+    }
+};
+
+
+TEST_F(command_handler_mqtt_fixture, main)
+{
+    test_v.push_back("wifi");
+    auto ret = test_chMQTT->run(test_v, &test_my_data);
+    EXPECT_STREQ(ret.c_str(), "done");
 }
