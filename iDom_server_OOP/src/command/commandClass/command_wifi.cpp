a9259a26b0c71979b44c43548e4891689a80990b
cyniu88 <cyniu88@wp.pl>
2019-05-12 21:38:08 +0200

    Revert "uusniecie const"
    This reverts commit 00e2427db1c284e3bd1592f3f0ed37c58af750b1.

--- iDom_server_OOP/src/command/commandClass/command_wifi.cpp
+++ iDom_server_OOP/src/command/commandClass/command_wifi.cpp
@@ -6,10 +6,11 @@ command_wifi::command_wifi(const std::string &name):command(name)
 
 std::string command_wifi::execute(std::vector<std::string> &v, thread_data *my_data)
 {
+    puts ("wifi command");
     return "done";
 }
 
-std::string command_wifi::help()
+std::string command_wifi::help() const
 {
     return "internal wifi command";
 }
