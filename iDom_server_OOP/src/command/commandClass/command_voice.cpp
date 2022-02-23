8813d705660f06f863d4cf1982138802969e5485
cyniu88 <cyniu88@wp.pl>
2022-02-23 13:41:20 +0100

    dodano salon  i gaszenie w calym domu

--- iDom_server_OOP/src/command/commandClass/command_voice.cpp
+++ iDom_server_OOP/src/command/commandClass/command_voice.cpp
@@ -12,23 +12,32 @@ std::string command_voice::execute(std::vector<std::string> &v, thread_data *my_
         useful_F_libs::toLower(vv);
     }
 
-    if(v[0] == "włącz"){
+    if(v[0] == "włącz" and v.size() > 3){
         if(v[1] == "radio"){
             my_data->main_iDomTools->MPD_play(my_data);
             return "done";
         }
-        else if(v[1] == "światło" and v[3] == "biurze"){
-            my_data->main_house_room_handler->turnOnAllInRoom("pokoj_E");
+        else if(v[1] == "światło"){
+            if(v[3] == "biurze")
+                my_data->main_house_room_handler->turnOnAllInRoom("pokoj_E");
+            else if( v[3] == "salonie")
+                my_data->main_house_room_handler->turnOnAllInRoom("salon");
+
             return "done";
         }
     }
-    else if(v[0] == "wyłącz"){
+    else if(v[0] == "wyłącz" and v.size() > 3){
         if(v[1] == "radio"){
             my_data->main_iDomTools->MPD_stop();
             return "done";
         }
-        else if(v[1] == "światło" and v[3] == "biurze"){
-            my_data->main_house_room_handler->turnOffAllInRoom("pokoj_E");
+        else if(v[1] == "światło"){
+            if(v[3] == "biurze")
+                my_data->main_house_room_handler->turnOffAllInRoom("pokoj_E");
+            else if(v[3] == "salonie")
+                my_data->main_house_room_handler->turnOffAllInRoom("salon");
+            else if (v[3] == "wszędzie")
+                my_data->main_house_room_handler->turnOffAllBulb();
             return "done";
         }
     }
