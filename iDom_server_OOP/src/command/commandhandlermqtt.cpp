ffe1b242c5acf915b7d17912e7c4e39f30eb8b03
cyniu88 <cyniu88@wp.pl>
2022-02-23 10:33:09 +0100

    Revert "poczatek sterowania glosem po mqtt"
    This reverts commit 5508988a086a327bb5abb5ea1ade6ca1b5a25da5.

--- iDom_server_OOP/src/command/commandhandlermqtt.cpp
+++ iDom_server_OOP/src/command/commandhandlermqtt.cpp
@@ -9,18 +9,16 @@
 #include "commandClass/command_buderus.h"
 #include "commandClass/command_light.h"
 #include "commandClass/command_idom.h"
-#include "commandClass/command_wylacz.h"
-#include "commandClass/command_wlacz.h"
 
 CommandHandlerMQTT::CommandHandlerMQTT()
 {
-    std::unique_ptr <command> r_433MHz (new command_433MHz("433mhz"));
+    std::unique_ptr <command> r_433MHz (new command_433MHz("433MHz"));
     commandMap.insert(std::make_pair(r_433MHz->getCommandName(), std::move(r_433MHz)));
 
-    std::unique_ptr <command> MPD(new command_mpd("mpd"));
+    std::unique_ptr <command> MPD(new command_mpd("MPD"));
     commandMap.insert(std::make_pair(MPD->getCommandName(), std::move (MPD)));
 
-    std::unique_ptr <command> RS232 (new commandRS232("rs232"));
+    std::unique_ptr <command> RS232 (new commandRS232("RS232"));
     commandMap.insert(std::make_pair(RS232->getCommandName(), std::move(RS232)));
 
     std::unique_ptr <command> log (new command_log("log"));
@@ -38,27 +36,12 @@ CommandHandlerMQTT::CommandHandlerMQTT()
     std::unique_ptr <command> light (new command_light("light"));
     commandMap.insert(std::make_pair(light->getCommandName(), std::move(light)));
 
-    std::unique_ptr <command> idom (new command_iDom("idom"));
+    std::unique_ptr <command> idom (new command_iDom("iDom"));
     commandMap.insert(std::make_pair(idom->getCommandName(), std::move(idom)));
-
-    std::unique_ptr <command> wlacz (new command_iDom("włącz"));
-    commandMap.insert(std::make_pair(wlacz->getCommandName(), std::move(wlacz)));
-
-    std::unique_ptr <command> wylacz (new command_iDom("wyłącz"));
-    commandMap.insert(std::make_pair(wylacz->getCommandName(), std::move(wylacz)));
 }
 
 std::string CommandHandlerMQTT::run(std::vector<std::string> &v, thread_data *my_data)
 {
-    //
-   // for(auto &vv : v)
-    {
-        for(auto &z : v[0]){
-            if(z >= 'A' && z <= 'Z') //gdy napotkamy małą literę zamieniamy ją na dużą
-                z += 32; //przesuwając o 32 miejsca w kodzie ASCII
-        }
-    }
-
     if (commandMap.find(v[0]) == commandMap.end()){
         std::fstream log;
         log.open( "/mnt/ramdisk/command.txt", std::ios::binary | std::ios::in | std::ios::out|std::ios::app );
