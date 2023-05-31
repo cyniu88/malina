688510cfea6870113fbd06d0eef1f49e97664c0e
cyniu88 <bazz5@wp.pl>
2023-05-31 19:53:23 +0000

    zmana koko

--- koko/main.cpp
+++ koko/main.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 #include <memory>
 #include <unordered_map>
-/*#include <source_location>
+#include <experimental/source_location>
 #include <string_view>
 
 void log(const std::string_view message,
-         const std::source_location location = 
-               std::source_location::current())
+         const std::experimental::fundamentals_v2::source_location location = 
+               std::experimental::fundamentals_v2::source_location::current())
 {
     std::cout << "file: "
               << location.file_name() << "("
@@ -20,24 +20,12 @@ void log(const std::string_view message,
 template <typename T> void fun(T x)
 {
     log(x);
-}*/
+}
 
 int main(int, char *[])
 {
-    //    log("Hello world!");
-    //    fun("Hello C++20!");
+        log("Hello world!");
+        fun("Hello C++20!");
 
-    std::unordered_map<std::string, std::string> mapa;
-    mapa["dupa"] = "dupa";
 
-    auto lambda = [](const std::unordered_map<std::string, std::string> &mapa) {
-        std::cout << "mamy: " << mapa.at("dupa") << std::endl;
-        ;
-    };
-
-    std::cout << mapa.size() << std::endl;
-    auto bb = std::bind(lambda, mapa);
-    bb();
-
-    std::cout << mapa.size() << std::endl;
 }
