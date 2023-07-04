e46a4898d5da8c74635ae7a9fdc918de1322b740
cyniu88 <cyniu88@wp.pl>
2023-07-04 16:04:04 +0200

    fix szybki

--- iDom_server_OOP/src/LCD_c/lcd_c.h
+++ iDom_server_OOP/src/LCD_c/lcd_c.h
@@ -9,7 +9,7 @@
 #include "../../liquidcrystal_i2c-for-ardupi/LiquidCrystal_I2C.h"
 #include "../idom_api.h"
 
-class LCD_c //: public iDom_API
+class LCD_c : public iDom_API
 {
     // przyklad ze strony "arduinowo.pl"
     unsigned char arrowDown[8] = {
@@ -42,7 +42,7 @@ public:
     void scrollRight();
     void printString(const std::string &row1, const std::string &row2, bool clear = false);
     std::string getData() const;
-   // std::string dump() const override;
+    std::string dump() const override;
 
 private:
     std::string m_printed = "NULL";
