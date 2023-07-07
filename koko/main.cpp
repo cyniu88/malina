1de7e9074a14ade8a9ad7d27d549b145c65efa24
cyniu88 <cyniu88@wp.pl>
2023-07-07 10:49:32 +0200

    new koko

--- koko/main.cpp
+++ koko/main.cpp
@@ -1,9 +1,18 @@
+
+/*
 #include <functional>
 #include <iostream>
 #include <memory>
 #include <unordered_map>
 #include <experimental/source_location>
 #include <string_view>
+
+#include <sys/ioctl.h>
+#include <linux/if.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
 
 void log(const std::string_view message,
          const std::experimental::fundamentals_v2::source_location location = 
@@ -26,6 +35,122 @@ int main(int, char *[])
 {
         log("Hello world!");
         fun("Hello C++20!");
+    int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
 
+    struct ifreq ifr{};
+    strcpy(ifr.ifr_name, "wlo1");
+    ioctl(fd, SIOCGIFADDR, &ifr);
+    close(fd);
 
+    char ip[INET_ADDRSTRLEN];
+    strcpy(ip, inet_ntoa(((sockaddr_in *) &ifr.ifr_addr)->sin_addr));
+
+    std::cout << ip << std::endl;
+
+    return 0;
+}
+
+*/
+#include <arpa/inet.h>
+#include <cerrno>
+#include <ifaddrs.h>
+#include <iostream>
+#include <net/if.h>
+#include <string>
+#include <string.h>
+#include <sysexits.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main(void)
+{
+    struct ifaddrs* ptr_ifaddrs = nullptr;
+
+    auto result = getifaddrs(&ptr_ifaddrs);
+    if( result != 0 ){
+        std::cout << "`getifaddrs()` failed: " << strerror(errno) << std::endl;
+
+        return EX_OSERR;
+    }
+
+    for(
+        struct ifaddrs* ptr_entry = ptr_ifaddrs;
+        ptr_entry != nullptr;
+        ptr_entry = ptr_entry->ifa_next
+    ){
+        std::string ipaddress_human_readable_form;
+        std::string netmask_human_readable_form;
+
+        std::string interface_name = std::string(ptr_entry->ifa_name);
+        sa_family_t address_family = ptr_entry->ifa_addr->sa_family;
+        if( address_family == AF_INET ){
+            // IPv4
+
+            // Be aware that the `ifa_addr`, `ifa_netmask` and `ifa_data` fields might contain nullptr.
+            // Dereferencing nullptr causes "Undefined behavior" problems.
+            // So it is need to check these fields before dereferencing.
+            if( ptr_entry->ifa_addr != nullptr ){
+                char buffer[INET_ADDRSTRLEN] = {0, };
+                inet_ntop(
+                    address_family,
+                    &((struct sockaddr_in*)(ptr_entry->ifa_addr))->sin_addr,
+                    buffer,
+                    INET_ADDRSTRLEN
+                );
+
+                ipaddress_human_readable_form = std::string(buffer);
+            }
+
+            if( ptr_entry->ifa_netmask != nullptr ){
+                char buffer[INET_ADDRSTRLEN] = {0, };
+                inet_ntop(
+                    address_family,
+                    &((struct sockaddr_in*)(ptr_entry->ifa_netmask))->sin_addr,
+                    buffer,
+                    INET_ADDRSTRLEN
+                );
+
+                netmask_human_readable_form = std::string(buffer);
+            }
+
+            std::cout << interface_name << ": IP address = " << ipaddress_human_readable_form << ", netmask = " << netmask_human_readable_form << std::endl;
+        }
+        else if( address_family == AF_INET6 ){
+            // IPv6
+            uint32_t scope_id = 0;
+            if( ptr_entry->ifa_addr != nullptr ){
+                char buffer[INET6_ADDRSTRLEN] = {0, };
+                inet_ntop(
+                    address_family,
+                    &((struct sockaddr_in6*)(ptr_entry->ifa_addr))->sin6_addr,
+                    buffer,
+                    INET6_ADDRSTRLEN
+                );
+
+                ipaddress_human_readable_form = std::string(buffer);
+                scope_id = ((struct sockaddr_in6*)(ptr_entry->ifa_addr))->sin6_scope_id;
+            }
+
+            if( ptr_entry->ifa_netmask != nullptr ){
+                char buffer[INET6_ADDRSTRLEN] = {0, };
+                inet_ntop(
+                    address_family,
+                    &((struct sockaddr_in6*)(ptr_entry->ifa_netmask))->sin6_addr,
+                    buffer,
+                    INET6_ADDRSTRLEN
+                );
+
+                netmask_human_readable_form = std::string(buffer);
+            }
+
+            std::cout << interface_name << ": IP address = " << ipaddress_human_readable_form << ", netmask = " << netmask_human_readable_form << ", Scope-ID = " << scope_id << std::endl;
+        }
+        else {
+            // AF_UNIX, AF_UNSPEC, AF_PACKET etc.
+            // If ignored, delete this section.
+        }
+    }
+
+    freeifaddrs(ptr_ifaddrs);
+    return EX_OK;
 }
