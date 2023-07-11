4270987b667a6b36ecc7ec715d21d571e51f97d1
dockerCyniu <dockerCyniu@wp.pl>
2023-07-11 15:40:06 +0200

    poprawka SerialPi

--- iDom_server_OOP/src/SerialPi/serialpi.cpp
+++ iDom_server_OOP/src/SerialPi/serialpi.cpp
@@ -8,33 +8,33 @@ SerialPi::SerialPi(const std::string &address) : m_address(address), m_serial_po
 
 SerialPi::~SerialPi()
 {
-  //  serialClose(m_serial_port);
+    serialClose(m_serial_port);
 }
 
 void SerialPi::begin(int budrate)
 {
-   // if ((m_serial_port = serialOpen(m_address.c_str(), budrate)) < 0) /* open serial port */
-   // {
-   //     throw "cannot open serial port"; // todo add exceptions
-   // }
+    if ((m_serial_port = serialOpen(m_address.c_str(), budrate)) < 0) /* open serial port */
+    {
+        throw "cannot open serial port"; // todo add exceptions
+    }
 }
 
 int SerialPi::available()
 {
-    return 0;//serialDataAvail(m_serial_port);
+    return serialDataAvail(m_serial_port);
 }
 
 char SerialPi::read()
 {
-    return 'o';//static_cast<char>(serialGetchar(m_serial_port));
+    return static_cast<char>(serialGetchar(m_serial_port));
 }
 
 void SerialPi::flush()
 {
-    //serialFlush(m_serial_port);
+    serialFlush(m_serial_port);
 }
 void SerialPi::print(const std::string &m)
 {
-    //std::lock_guard<std::mutex> lock(mutex_rs232);
-   /// serialPrintf(m_serial_port, m.c_str());
+    std::lock_guard<std::mutex> lock(mutex_rs232);
+    serialPrintf(m_serial_port, m.c_str());
 }
